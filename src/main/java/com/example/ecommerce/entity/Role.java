2dac5715ac525624787f5846faff95ba8fef093f
Abishekds2233 <115973480+Abishekds2233@users.noreply.github.com>
2026-07-08 22:03:38 +0530

    home commit

--- src/main/java/com/example/ecommerce/entity/Role.java
+++ src/main/java/com/example/ecommerce/entity/Role.java
@@ -1,5 +1,8 @@
 package com.example.ecommerce.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,7 +14,16 @@ public class Role {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name ="role_id")
 	private Long id;
+	
+	@Column(name ="role_name")
 	private String name;
+	
+	private String description;
+	@Column(name ="created_at")
+	private LocalDateTime createdat;
+	@Column(name ="updated_at")
+	private LocalDateTime updatedat;
 
 }
