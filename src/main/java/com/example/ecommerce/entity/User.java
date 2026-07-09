ebc15055cd4411044446716b346e10ddb5fd19c2
MaheshRooks <mahesh.rooks@gmail.com>
2026-07-09 17:55:53 +0530

    latest commit

--- src/main/java/com/example/ecommerce/entity/User.java
+++ src/main/java/com/example/ecommerce/entity/User.java
@@ -1,5 +1,30 @@
 package com.example.ecommerce.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+
 public class User {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name= "user_id")
+	private Long userid;
+	
+	@OneToMany
+	@JoinColumn(name = "role_id")
+	private Role role;
+	
+	private String name;
+	
+	@NotBlank
+	private String email;
+	
+	
 
 }
