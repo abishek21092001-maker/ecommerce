a5ee99dfdc676e267c7469683979d18ff4b0f75d
MaheshRooks <mahesh.rooks@gmail.com>
2026-07-17 17:02:33 +0530

    latest commit

--- src/main/java/com/example/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/example/ecommerce/repository/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.example.ecommerce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.ecommerce.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product,Long>{
+	List<Product> findByNameContainingIgnoreCase(String name);
+	
+
 
 }
